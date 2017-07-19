6179bebe6868921321d54b124beeb974f2e13eb6
Delwar <twinkle_023020@yahoo.com>
2017-07-19 21:04:54 +0600

    initial project

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    alert('hello');
+  }, false);
+}, false);
